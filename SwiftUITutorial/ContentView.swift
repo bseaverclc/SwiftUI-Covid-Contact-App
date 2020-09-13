62844c18b41f0fbdb0918e14ac2e4471c1911387
Brian Seaver <bseaver@d155.org>
2020-09-13 15:33:24 -0500

    Initial Commit

---
+++ SwiftUITutorial/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUITutorial
+//
+//  Created by Brian Seaver on 9/13/20.
+//  Copyright © 2020 clc.seaver. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
